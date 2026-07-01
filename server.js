05b585851b7a9122dc793744dcc4468729664779
ejabbing <ernestjabea@gmail.com>
2026-07-01 17:41:51 +0100

    fix(security): add trust proxy to fix client IP resolution behind Nginx

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', true);
 const PORT = process.env.PORT || 3002;
 
 for (const requiredSecret of ['JWT_SECRET', 'JWT_REFRESH_SECRET']) {
